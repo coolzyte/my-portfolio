e0b06d9577dca9074dde5659a0b194fc32d99a97
coolzyte <coolzyte@gmail.com>
2018-11-14 11:59:17 +0100

    Added google map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import Aux from "../../hoc/Aux/Aux";
 
-const Map = props => (
-  <div className="google-map">
-    <div id="map" />
-  </div>
-);
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 7.41,
+      lng: 3.95
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <Aux>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyABH2bRj1AS84QRvZWJ7osgmMIdVZdfwbE" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={7.410979}
+            lng={3.951922}
+            text={"Kreyser Avrora"}
+          />
+        </GoogleMapReact>
+      </Aux>
+    );
+  }
+}
 
 export default Map;
